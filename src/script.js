88f6017d7daff262f74bf8b87b8045c022179d75
iyed <iyed.mohamed@isimg.tn>
2024-10-05 15:01:16 +0100

    update done

--- src/script.js
+++ src/script.js
@@ -5,6 +5,10 @@ const todoAlert = document.getElementById("Alert");
 const listItems = document.getElementById("list-items");
 const addUpdate = document.getElementById("addClick");
 
+function setLocalStorage() {
+    localStorage.setItem("todo-list", JSON.stringify(todo));
+}
+
 function setAlertMessage(message, color = "red") {
     todoAlert.innerText = message; 
     todoAlert.style.color = color; // Change la couleur du message (par défaut rouge)
@@ -45,4 +49,65 @@ function createItem() {
         todo.push(itemList);
         setLocalStorage();
     }
+}
+
+function readTodoItems() {
+    todo.forEach((element) => {
+        let li = document.getElementById("li");
+        let style ="";
+        if ( element.status) {
+            style="style='text-decorations: line-through'";
+        }
+        const todoItems = `<div ${style} title="Hit Double Click and Complete" ondbclick="CompletedToDoItems(this)"> ${element.item} 
+            ${
+                style === ""
+                ? ' <img class="edit todo-controls" onclick="UpdateToDoItems(this) src="//images/pencil.png" />'
+                : ""
+                }
+                <img class="edit todo-controls" onclick="DeleteToDoItems(this) src="//images/delete.png" /> </div> </div>`;
+        li.innerHTML = todoItems;
+        listItems.appendChild(li);
+    });
+}
+readTodoItems();
+
+function UpdateToDoItems(e) {
+    if (
+        e.parentElement.parentElement.querySelector("div").style.textDecoration === ""
+    ) {
+        todoValue.value = 
+        e.parentElement.parentElement.querySelector("div").innerText;
+        updateText = e.parentElement.parentElement.querySelector("div");
+        addUpdate.setAttribute("onclick", "UpdateOnSelectionItems()");
+        addUpdate.setAttribute("src", "//images/refresh.png");
+        todoValue.focus() ;
+    }
+}
+
+function UpdateOnSelectionItems() {
+    let isPresent = false;
+    todo.forEach((element) => {
+        if (element.item === todoValue.value) {
+            isPresent = true;
+        }
+
+    });
+
+    if(isPresent) {
+        setAlertMessage("This item is already exists");
+        return;
+    }
+
+    todo.forEach((element) => {
+        if (element.item == updateText.innerText.trim()) {
+            element.item = todoValue.value;
+        }
+    });
+    setLocalStorage();
+    updateText.innerText = todoValue.value;
+    addUpdate.setAttribute("onclick", "CreateItem()");
+    addUpdate.setAttribute("src", "//images/add.png");
+    todoValue.value = "";
+    setAlertMessage("ToDo item updated successfully","blue");
+
 }
